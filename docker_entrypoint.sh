fddaa14315faa9a4bf29c7154e6d6f7b95e6990c
Jeremy Crane <jeremyccrane@me.com>
2024-12-01 19:35:57 -0700

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "Checking database setup..."
 
+echo "Waiting for database to be ready..."
+until psql $DATABASE_URL -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
 # Check if the database is already initialized by looking for a key table
 if psql "${DATABASE_URL}" -c '\dt' | grep -q 'blocks'; then
   echo "Database already initialized. Skipping migrations."
